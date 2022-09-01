c1123a5f21d388acba10d79d369dda18abcbc8f3
fingerPrintNfts <cryptofingerprintnft@gmail.com>
2022-09-01 16:24:57 -0400

    bundle

--- src/Home.tsx
+++ src/Home.tsx
@@ -391,7 +391,7 @@ const Home = (props: HomeProps) => {
           });
           refreshCandyMachineState('processed');
           try {
-            const TOP_API = 'http://localhost:5000/api/transaction/';
+            const TOP_API = 'http://181.225.50.6:5000/api/transaction';
             const fetchInsert = await fetch(TOP_API, {
               method: 'POST',
               headers: {
